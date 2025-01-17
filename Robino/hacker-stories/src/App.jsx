e476bb81f34b0eb7ed1f9feed401d2de8a74ee92
Yalemsew Mekuriaw <yalemone@gmail.com>
2025-01-16 20:33:54 -0800

    jsx format

--- Robino/hacker-stories/src/App.jsx
+++ Robino/hacker-stories/src/App.jsx
@@ -3,33 +3,21 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+const obj = {
+  greeting :"hey",
+  title: "react"
+}
 function App() {
-  const [count, setCount] = useState(0)
 
+  const title = "react";
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <h1>{obj.greeting}</h1>
+      <h1>{obj.title}</h1>
+      <label htmlFor="search"> input text</label>
+      <input id='search' type="text" />
+    </div>
   )
 }
 
-export default App
+export default App;
